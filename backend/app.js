d23786df06fc85779e09966d9d99aad728492f85
Sergey Firsov <Sergey.firsov.m2@yandex.ru>
2020-12-04 16:45:28 +0300

    Деплой/12

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+//const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -31,12 +31,12 @@ app.use(cors());
 
 app.use(requestLogger);
 
-const limiter = rateLimit({
+/*const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 100,
   message: 'Вы превысили количество запросов',
 });
-app.use(limiter);
+app.use(limiter);*/
 
 app.use('/', usersRouter);
 
